f1204e0bfe373e61f8bca151f9f7acc480a80573
Kivi Corn <kivi.webdev@gmail.com>
2024-03-01 15:53:32 +0000

    create Firestore document. Invite players

--- src/components/ui/create-room-dialog.tsx
+++ src/components/ui/create-room-dialog.tsx
@@ -1,9 +1,16 @@
 'use client'
-import { Dialog, DialogActions, DialogContent, DialogTitle, TextField } from "@mui/material";
+import { useRouter } from 'next/navigation'
+import { db } from "@/lib/firebase/config";
+import { Backdrop, CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from "@mui/material";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { useState } from "react";
 
 const CreateRoomDialog = () => {
   const [dialogOpen, setDialogOpen] = useState(false)
+  const [displayName, setDisplayName] = useState('')
+  const [pending, setPending] = useState(false)
+  const [error, setError] = useState(false)
+  const router = useRouter()
 
   function handleOpenDialog() {
     setDialogOpen(true)
@@ -11,6 +18,33 @@ const CreateRoomDialog = () => {
 
   function handleCloseDialog() {
     setDialogOpen(false)
+  }
+
+  async function handleSubmit() {
+    setPending(true)
+    setDialogOpen(false)
+    try {
+      // CREATE A NEW FIRESTORE DOCUMENT
+      const userId = crypto.randomUUID()
+      const docRef = await addDoc(collection(db, 'rooms'), {
+        timestamp: serverTimestamp(),
+        user: [
+          {
+            name: displayName,
+            id: userId,
+            roomCreator: true
+          }
+        ]
+      });
+      localStorage.setItem("user_id", userId)
+      // If document creation is successful, navigate to the created room
+      router.push(`/room/${docRef.id}`);
+    } catch (error) {
+      // Handle any errors that occur during document creation
+      console.error('Error creating document:', error);
+      setError(true)
+      setPending(false)
+    }
   }
 
   return ( 
@@ -35,25 +69,55 @@ const CreateRoomDialog = () => {
             autoFocus
             required
             margin="dense"
-            id="name"
-            name="email"
             label="Your display name"
             type="text"
             fullWidth
             variant="outlined"
             autoComplete="off"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
           />
         </DialogContent>
         <DialogActions sx={{ padding: 6 }}>
           <button
             type="submit"
             className="w-full px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-hippo-brand-green hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
-            onClick={handleOpenDialog}
+            onClick={handleSubmit}
+            disabled={pending}
           >
             Create room
           </button>
         </DialogActions>
       </Dialog>
+      {pending && (
+        <Backdrop
+          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={pending}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      )}
+      {error && (
+        <div className="absolute top-4 m-auto left-0 right-0 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow" role="alert">
+          <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg">
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
+            </svg>
+            <span className="sr-only">Error icon</span>
+          </div>
+          <div className="ms-3 text-sm font-normal">An error occurred.</div>
+          <button
+            type="button"
+            className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8"
+            onClick={() => setError(false)}
+          >
+            <span className="sr-only">Close</span>
+            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+          </button>
+        </div>
+      )}
     </>
   );
 }
---
+++ src/components/ui/share-room-dialog.tsx
@@ -0,0 +1,109 @@
+'use client'
+import { Dialog, DialogActions, DialogContent, DialogTitle, TextField } from "@mui/material";
+import { useState } from "react";
+import { usePathname } from 'next/navigation'
+import { useCopyToClipboard } from "@/hooks/useCopyToClipboard";
+import QRCode from "react-qr-code";
+
+const ShareRoomDialog = () => {
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const [copiedText, copy] = useCopyToClipboard()
+  const [copiedAlertOpen, setCopiedAlertOpen] = useState(false)
+  const pathname = usePathname()
+  const currentURL = `${process.env.NEXT_PUBLIC_URL}${pathname}`
+
+  const handleCopyLink = () => {
+    copy(currentURL)
+      .then(() => {
+        setCopiedAlertOpen(true)
+        setDialogOpen(false)
+      })
+      .catch(error => {
+        console.error('Failed to copy!', error)
+      })
+  }
+
+  function handleOpenDialog() {
+    setDialogOpen(true)
+  }
+
+  function handleCloseDialog() {
+    setDialogOpen(false)
+  }
+
+  return ( 
+    <>
+    <button
+      type="button"
+      className="inline-flex items-center text-hippo-brand-navy border border-hippo-brand-navy bg-gray-100 hover:bg-white focus:ring-1 focus:outline-none focus:ring-hippo-brand-navy font-medium rounded-lg text-sm px-5 py-3 text-center me-2 mb-2"
+      onClick={handleOpenDialog}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+      </svg>
+      Invite players
+    </button>
+      <Dialog
+        open={dialogOpen}
+        onClose={handleCloseDialog}
+        disableRestoreFocus
+        fullWidth
+      >
+        <DialogTitle sx={{ fontSize: 26, padding: 6 }}>Invite players</DialogTitle>
+        <DialogContent sx={{ paddingX: 6, paddingY: 0 }}>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            label="Game's url"
+            type="text"
+            fullWidth
+            variant="outlined"
+            autoComplete="off"
+            value={currentURL}
+          />
+          <QRCode
+            style={{ height: "auto", maxWidth: "160px", width: "100%", margin: "auto", paddingTop: "24px" }}
+            value={currentURL}
+            // size={100}
+            // // viewBox={`0 0 256 256`}
+          />
+        </DialogContent>
+        <DialogActions sx={{ padding: 6 }}>
+          <button
+            type="submit"
+            className="w-full px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-hippo-brand-navy hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            onClick={handleCopyLink}
+          >
+            Copy invitation link
+          </button>
+        </DialogActions>
+      </Dialog>
+      {copiedAlertOpen && copiedText && (
+        <div className="absolute top-4 m-auto left-0 right-0 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow" role="alert">
+          <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+            </svg>
+            <span className="sr-only">Check icon</span>
+          </div>
+          <div className="ms-3 text-sm font-normal">Invitation link copied to clipboard!</div>
+          <button
+            type="button"
+            className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+            data-dismiss-target="#toast-success"
+            aria-label="Close"
+            onClick={() => setCopiedAlertOpen(false)}
+          >
+            <span className="sr-only">Close</span>
+            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+          </button>
+        </div>
+      )}
+    </>
+  );
+}
+ 
+export default ShareRoomDialog;
